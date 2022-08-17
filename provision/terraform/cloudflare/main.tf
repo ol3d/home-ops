2c6af20acebf389450220a895ab2fdfdb6b5709c
Adam Delo <deloa6133@gmail.com>
2022-08-17 14:51:59 +0000

    Create base terraform cloudflare structure

---
+++ provision/terraform/cloudflare/main.tf
@@ -0,0 +1,39 @@
+terraform {
+
+    backend "remote" {
+        organization = ""
+        workspaces {
+            name = ""
+        }
+    }
+
+    required_providers {
+        cloudflare = {
+            source  = "cloudflare/cloudflare"
+            version = "3.21.0"
+        }
+        http = {
+            source  = "hashicorp/http"
+            version = "3.0.1"
+        }
+        sops = {
+            source  = "carlpett/sops"
+            version = "0.7.1"
+        }
+    }
+}
+
+data "sops_file" "cloudflare_secrets" {
+    source_file = "secret.sops.yaml"
+}
+
+provider "cloudflare" {
+    email   = data.sops_file.cloudflare_secrets.data["cloudflare_email"]
+    api_key = data.sops_file.cloudflare_secrets.data["cloudflare_apikey"]
+}
+
+data "cloudflare_zones" "domain" {
+    filter {
+        name = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
+    }
+}
