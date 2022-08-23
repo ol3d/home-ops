c1be451ab74f9d4f0765b9513dd82323933428dd
Adam Delo <deloa6133@gmail.com>
2022-08-23 19:51:41 +0000

    implement meta-labels into workflow process

---
+++ provision/terraform/backblaze/main.tf
@@ -0,0 +1,35 @@
+terraform {
+
+    backend "s3" {
+        bucket = "homeops-tf-state"
+        key = "backblaze/terraform.tfstate"
+        region = "us-east-1"
+        shared_credentials_file = "~/.aws/credentials"
+        profile = "terraform"
+        encrypt = true
+    }
+
+    required_providers {
+        backblaze = {
+            source  = "Backblaze/b2"
+            version = "0.8.1"
+        }
+        http = {
+            source  = "hashicorp/http"
+            version = "3.0.1"
+        }
+        sops = {
+            source  = "carlpett/sops"
+            version = "0.7.1"
+        }
+    }
+}
+
+data "sops_file" "backblaze_secrets" {
+    source_file = "secret.sops.yaml"
+}
+
+provider "b2" {
+    application_key = ""
+    application_key_id = ""
+}
