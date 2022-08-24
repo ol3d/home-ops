359ff2778e57a32ec6c9e78b836f5159799f9947
Adam Delo <deloa6133@gmail.com>
2022-08-24 21:40:47 +0000

    test renovate versioning

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -16,7 +16,7 @@ terraform {
         }
         http = {
             source  = "hashicorp/http"
-            version = "3.0.1"
+            version = "3.0.0"
         }
         sops = {
             source  = "carlpett/sops"
