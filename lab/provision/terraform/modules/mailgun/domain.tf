bd4494dc4ef35300be0e5390e8b0de76e7f0911a
Adam Delo <deloa6133@gmail.com>
2023-12-07 09:53:06 -0600

    opnsense initial commit

--- lab/provision/terraform/modules/mailgun/domain.tf
+++ lab/provision/terraform/modules/mailgun/domain.tf
@@ -1,11 +1,11 @@
 resource "mailgun_domain" "domain" {
     name = "mailgun.ol3d.dev"
     region = "us"
-    smtp_password = ""
+    smtp_password = data.sops_file.mailgun_secrets.data["domain-credential.password"]
     spam_action = "disabled"
     wildcard = false
     dkim_key_size = "2048"
-    dkim_selector = ""
+    dkim_selector = "krs"
     force_dkim_authority = false
-    open_tracking = "no"
+    open_tracking = false
 }
