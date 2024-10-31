9b1d1e32fb7b39a5599abf719b4a5696e733646d
Adam Delo <deloa6133@gmail.com>
2024-10-30 21:33:12 -0500

    terraform module cloudflare and mailgun update
    Modified cloudflare txt record for mailgun due to domain update on
    system. Additionally, prevented mailgun domain-credential resources from
    being created until domain name is valid.

--- lab/provision/terraform/modules/mailgun/domain-credential.tf
+++ lab/provision/terraform/modules/mailgun/domain-credential.tf
@@ -1,12 +1,12 @@
 resource "mailgun_domain_credential" "domain-credential-duplicacy" {
-  domain = "mailgun.ol3d.dev"
+  domain = mailgun_domain.domain.name
   login = "duplicacy"
   password = data.sops_file.mailgun_secrets.data["domain-credential.password"]
   region = "us"
 }
 
 resource "mailgun_domain_credential" "domain-credential-proxmox" {
-  domain = "mailgun.ol3d.dev"
+  domain = mailgun_domain.domain.name
   login = "proxmox"
   password = data.sops_file.mailgun_secrets.data["domain-credential.password"]
   region = "us"
