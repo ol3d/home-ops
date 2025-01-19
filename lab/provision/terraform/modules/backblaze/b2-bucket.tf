4801b71534204a975dc2b4b324446495294b428a
Adam Delo <deloa6133@gmail.com>
2025-01-19 11:39:56 -0600

    terraform backblaze b2 import bucket to local state
    Imported homeops-backups bucket to local terraform state file.
    Additionally, removed the for_each since there is currently only a
    single bucket being used. This made it easier for importing and
    management. Due to this, was able to remove the variables file
    completely.

--- lab/provision/terraform/modules/backblaze/b2-bucket.tf
+++ lab/provision/terraform/modules/backblaze/b2-bucket.tf
@@ -1,26 +1,19 @@
-resource "b2_bucket" "b2_bucket" {
-  for_each = var.b2_buckets
-
-  bucket_name = each.key
-  bucket_type = each.value.bucket_type
+resource "b2_bucket" "homeops-backups" {
+  bucket_name = "homeops-backups"
+  bucket_type = "allPrivate"
 
   file_lock_configuration {
     default_retention {
-      mode = each.value.file_lock_configuration.default_retention.mode
+      mode = "governance"
       period {
-        duration = each.value.file_lock_configuration.default_retention.period.duration
-        unit = each.value.file_lock_configuration.default_retention.period.unit
+        duration = 7
+        unit = "days"
       }
     }
-    is_file_lock_enabled = each.value.file_lock_configuration.is_file_lock_enabled
+    is_file_lock_enabled = true
   }
-
   lifecycle_rules {
-    file_name_prefix = each.value.lifecycle_rules.file_name_prefix
-    days_from_hiding_to_deleting = each.value.lifecycle_rules.days_from_hiding_to_deleting
-  }
-
-  lifecycle {
-    prevent_destroy = true
+    file_name_prefix = ""
+    days_from_hiding_to_deleting = 28
   }
 }
--- lab/provision/terraform/modules/backblaze/main.tf
+++ lab/provision/terraform/modules/backblaze/main.tf
@@ -1,5 +1,4 @@
 terraform {
-
   required_providers {
     b2 = {
       source  = "Backblaze/b2"
